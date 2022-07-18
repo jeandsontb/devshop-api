bc16a148acdf9085cabe9672023ebe9a699ea3bf
jeandson <jeandsontb@gmail.com>
2022-07-18 18:37:27 -0300

    implements graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+    }),
+    CategoryModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
