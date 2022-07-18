cf3e3fd92f7a69513711dae105f5f8bea9388e88
jeandson <jeandsontb@gmail.com>
2022-07-18 19:22:44 -0300

    file .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,33 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoryModule } from './category/category.module';
-import { Category } from './category/entity/category.entity';
+// import { Category } from './category/entity/category.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: 'postgres://docker:123456@localhost:5432/devshop',
-      autoLoadEntities: true,
-      synchronize: true,
-      // entities: [Category],
-      // logging: true
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+        // logging: true
+      }),
     }),
 
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'schema.gql',
     }),
+
     CategoryModule,
   ],
   controllers: [AppController],
