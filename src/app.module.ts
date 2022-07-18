b818b4145cacd475df38f81a96d05b318010b651
jeandson <jeandsontb@gmail.com>
2022-07-18 19:14:22 -0300

    implements entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,23 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoryModule } from './category/category.module';
+import { Category } from './category/entity/category.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: 'postgres://docker:123456@localhost:5432/devshop',
+      autoLoadEntities: true,
+      synchronize: true,
+      // entities: [Category],
+      // logging: true
+    }),
+
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'schema.gql',
