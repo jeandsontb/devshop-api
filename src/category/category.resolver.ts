063ebb5f737aae6fa2eed76f421e59e3f37b1fd2
jeandson <jeandsontb@gmail.com>
2022-07-20 21:13:20 -0300

    implements update

--- src/category/category.resolver.ts
+++ src/category/category.resolver.ts
@@ -2,6 +2,7 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CategoryService } from './category.service';
 import { CategoryPublic } from './dto/category';
 import { CategoryCreateInput } from './dto/category-create.input';
+import { CategoryUpdateInput } from './dto/category-update.input';
 import { CategoryMapper } from './mapper/category.mapper';
 
 @Resolver((of) => CategoryPublic)
@@ -13,11 +14,23 @@ export class CategoryResolver {
     return this.categoryService.findAll();
   }
 
+  @Query((returns) => CategoryPublic, { name: 'getCategoryById' })
+  async getCategoryById(@Args('id') id: string): Promise<CategoryPublic> {
+    return this.categoryService.findById(id);
+  }
+
   @Mutation((returns) => CategoryPublic, { name: 'CategoryCreateInput' })
   async createCategory(
     @Args('input') input: CategoryCreateInput,
   ): Promise<CategoryPublic> {
     return this.categoryService.create(CategoryMapper.toEntity(input));
+  }
+
+  @Mutation((returns) => CategoryPublic, { name: 'updateCategory' })
+  async updateCategory(
+    @Args('input') input: CategoryUpdateInput,
+  ): Promise<CategoryPublic> {
+    return this.categoryService.update(input);
   }
 
   @Mutation((returns) => Boolean, { name: 'deleteCategory' })
