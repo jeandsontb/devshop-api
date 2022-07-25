e49212744c45467b05300532439f081c48d9641a
jeandson <jeandsontb@gmail.com>
2022-07-25 08:17:49 -0300

    implements validation

--- src/modules/product/dto/product-update.input.ts
+++ src/modules/product/dto/product-update.input.ts
@@ -1,19 +1,25 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsUUID, Length } from 'class-validator';
 
 @InputType()
 export class ProductUpdateInput {
   @Field()
+  @IsUUID()
   id: string;
 
   @Field()
+  @Length(3)
   name: string;
 
   @Field()
+  @Length(3)
   slug: string;
 
   @Field()
+  @Length(5)
   description: string;
 
   @Field()
+  @IsUUID()
   category: string;
 }
